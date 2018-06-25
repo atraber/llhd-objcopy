98b331c5178c5bbce1cee060bd062a1a10011a22
Andreas Traber <andytraber@gmail.com>
2018-06-25 14:12:52 +0200

    Current state

--- src/vlog/module.rs
+++ src/vlog/module.rs
@@ -1,55 +1,107 @@
-use std::rc::Rc;
-
+#[derive(Debug)]
 pub enum PortType {
 	Input,
 	Output,
 	InOut,
 }
 
+#[derive(Debug)]
 pub struct Port {
-	wire: Rc<Wire>,
+	id: usize,
+	wire: WireRef,
 	ty: PortType,
 }
 
+#[derive(Debug)]
+pub struct PortRef {
+	id: usize,
+}
+
+#[derive(Debug)]
 pub struct Wire {
+	id: usize,
 	name: String,
 }
 
-pub struct Assign {
-	lhs: Rc<Wire>,
-	rhs: Rc<Wire>,
+#[derive(Debug)]
+pub struct WireRef {
+	id: usize,
 }
 
+#[derive(Debug)]
+pub struct Assign {
+	id: usize,
+	lhs: WireRef,
+	rhs: WireRef,
+}
+
+#[derive(Debug)]
+pub struct AssignRef {
+	id: usize,
+}
+
+#[derive(Debug)]
 pub struct Instance {
-	inst_name: String,
-	module: Rc<Module>,
+	pub id: usize,
+	pub inst_name: String,
+	pub module: ModuleRef,
 }
 
 impl Instance {
-	pub fn new(inst_name : String, m : Rc<Module>) -> Instance {
+	pub fn new(inst_name : String, m : ModuleRef) -> Instance {
 		Instance {
+			id: 0, // TODO: Nope! Not the right way!
 			inst_name: inst_name,
 			module: m,
 		}
 	}
 }
 
+#[derive(Debug)]
+pub struct InstanceRef {
+	pub id: usize,
+}
+
+impl InstanceRef {
+	pub fn new(i : &Instance) -> InstanceRef {
+		InstanceRef {
+			id: i.id,
+		}
+	}
+}
+
+#[derive(Debug)]
 pub struct Module {
+	pub id: usize,
 	pub name: String,
 	pub ports: Vec<Port>,
-	pub wires: Vec<Rc<Wire>>,
+	pub wires: Vec<Wire>,
 	pub assigns: Vec<Assign>,
 	pub instances: Vec<Instance>,
 }
 
 impl Module {
-	pub fn new(name: String) -> Module {
+	pub fn new(name : String) -> Module {
 		Module {
+			id: 0, // TODO: Nope! Not the right way!
 			name: name,
 			ports: vec![],
 			wires: vec![],
-			assigns: vec![],
 			instances: vec![],
+			assigns: vec![],
+		}
+	}
+}
+
+#[derive(Debug)]
+pub struct ModuleRef {
+	pub id: usize,
+}
+
+impl ModuleRef {
+	pub fn new(m : &Module) -> ModuleRef {
+		ModuleRef {
+			id: m.id,
 		}
 	}
 }
