1fc05d22309cd2bb38abc0fb1102bc4b90e3f89b
Andreas Traber <andytraber@gmail.com>
2018-06-30 18:04:52 +0200

    First useful vlog output
    LLHD is parsed to the point that useful structural verilog output can be
    generated.
    
    Specifically, all structural constructs are supported. What is still
    missing are vector types and a lot of error reporting.

--- src/vlog/module.rs
+++ src/vlog/module.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 #[derive(Debug)]
 pub enum PortType {
 	Input,
@@ -5,11 +7,29 @@ pub enum PortType {
 	InOut,
 }
 
+pub type Type = Rc<TypeKind>;
+
+#[derive(Debug)]
+pub enum TypeKind {
+	Scalar,
+	Vector(usize, Type),
+}
+
 #[derive(Debug)]
 pub struct Port {
 	id: usize,
-	wire: WireRef,
-	ty: PortType,
+	pub wire: WireRef,
+	pub port_ty: PortType,
+}
+
+impl Port {
+	pub fn new(w: WireRef, port_ty: PortType) -> Port {
+		Port {
+			id: 0, // TODO: Nope! Not the right way!
+			wire: w,
+			port_ty: port_ty,
+		}
+	}
 }
 
 #[derive(Debug)]
@@ -17,10 +37,29 @@ pub struct PortRef {
 	id: usize,
 }
 
+impl PortRef {
+	pub fn new(p : &Port) -> PortRef {
+		PortRef {
+			id: p.id,
+		}
+	}
+}
+
 #[derive(Debug)]
 pub struct Wire {
 	id: usize,
-	name: String,
+	pub name: String,
+	pub ty: Type,
+}
+
+impl Wire {
+	pub fn new(name: String, ty: Type) -> Wire {
+		Wire {
+			id: 0, // TODO: Nope! Not the right way!
+			name: name,
+			ty: ty,
+		}
+	}
 }
 
 #[derive(Debug)]
@@ -28,16 +67,52 @@ pub struct WireRef {
 	id: usize,
 }
 
-#[derive(Debug)]
-pub struct Assign {
-	id: usize,
-	lhs: WireRef,
-	rhs: WireRef,
+impl WireRef {
+	pub fn new(w : &Wire) -> WireRef {
+		WireRef {
+			id: w.id,
+		}
+	}
+
+	pub fn from_id(id: usize) -> WireRef {
+		WireRef {
+			id: id,
+		}
+	}
 }
 
 #[derive(Debug)]
-pub struct AssignRef {
-	id: usize,
+pub struct Assign {
+	pub id: usize,
+	pub lhs: WireRef,
+	pub rhs: WireRef,
+}
+
+impl Assign {
+	pub fn new(lhs: WireRef, rhs: WireRef) -> Assign {
+		Assign {
+			id: 0, // TODO: Nope! Not the right way!
+			lhs: lhs,
+			rhs: rhs,
+		}
+	}
+}
+
+#[derive(Debug)]
+pub struct PortAssign {
+	pub id: usize,
+	pub lhs: PortRef,
+	pub rhs: WireRef,
+}
+
+impl PortAssign {
+	pub fn new(lhs: PortRef, rhs: WireRef) -> PortAssign {
+		PortAssign {
+			id: 0, // TODO: Nope! Not the right way!
+			lhs: lhs,
+			rhs: rhs,
+		}
+	}
 }
 
 #[derive(Debug)]
@@ -45,28 +120,26 @@ pub struct Instance {
 	pub id: usize,
 	pub inst_name: String,
 	pub module: ModuleRef,
+	// lhs is within this instance, rhs is within container of instance
+	pub port_assigns: Vec<PortAssign>,
 }
 
 impl Instance {
-	pub fn new(inst_name : String, m : ModuleRef) -> Instance {
+	pub fn new(inst_name: String, m: ModuleRef) -> Instance {
 		Instance {
 			id: 0, // TODO: Nope! Not the right way!
 			inst_name: inst_name,
 			module: m,
+			port_assigns: vec![],
 		}
 	}
-}
 
-#[derive(Debug)]
-pub struct InstanceRef {
-	pub id: usize,
-}
-
-impl InstanceRef {
-	pub fn new(i : &Instance) -> InstanceRef {
-		InstanceRef {
-			id: i.id,
-		}
+	pub fn port_assign_push(&mut self, a: PortAssign) -> () {
+		// TODO: check for duplicates
+		let id = self.port_assigns.len();
+		self.port_assigns.push(a);
+		let amut = self.port_assigns.get_mut(id).unwrap();
+		amut.id = id;
 	}
 }
 
@@ -90,6 +163,66 @@ impl Module {
 			instances: vec![],
 			assigns: vec![],
 		}
+	}
+
+	pub fn instance_push(&mut self, i: Instance) -> () {
+		// TODO: check for duplicates
+		let id = self.instances.len();
+		self.instances.push(i);
+		let imut = self.instances.get_mut(id).unwrap();
+		imut.id = id;
+	}
+
+	pub fn assign_push(&mut self, a: Assign) -> () {
+		// TODO: check for duplicates
+		let id = self.assigns.len();
+		self.assigns.push(a);
+		let amut = self.assigns.get_mut(id).unwrap();
+		amut.id = id;
+	}
+
+	pub fn wire_push(&mut self, w: Wire) -> WireRef {
+		// TODO: check for duplicates
+		let id = self.wires.len();
+		self.wires.push(w);
+		let wmut = self.wires.get_mut(id).unwrap();
+		wmut.id = id;
+		return WireRef::new(wmut);
+	}
+
+	pub fn port_push(&mut self, p: Port) -> PortRef {
+		// TODO: check for duplicates
+		let id = self.ports.len();
+		self.ports.push(p);
+		let pmut = self.ports.get_mut(id).unwrap();
+		pmut.id = id;
+		return PortRef::new(pmut);
+	}
+
+	pub fn port_get(&self, name: &String) -> Option<PortRef> {
+		for port in &self.ports {
+			if &self.wire(&port.wire).unwrap().name == name {
+				return Some(PortRef::new(&port));
+			}
+		}
+		return None;
+	}
+
+	pub fn wire_get(&self, name: &String) -> Option<WireRef> {
+		for wire in &self.wires {
+			if &wire.name == name {
+				return Some(WireRef::new(&wire));
+			}
+		}
+		return None;
+	}
+
+	pub fn port(&self, p_ref : &PortRef) -> Option<&Port> {
+		self.ports.get(p_ref.id)
+	}
+
+	pub fn wire(&self, w_ref : &WireRef) -> Option<&Wire> {
+		self.wires.get(w_ref.id)
 	}
 }
 
--- src/vlog/writer.rs
+++ src/vlog/writer.rs
@@ -1,15 +1,16 @@
 extern crate llhd;
+use llhd::unit::AsUnitContext;
 
 use std::error::Error;
 use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
+use std::rc::Rc;
 
 use vlog::module::*;
 
 struct VlogWriter {
 	modules: Vec<Module>,
-	instances: Vec<Instance>,
 	instance_counter : usize,
 }
 
@@ -17,7 +18,6 @@ impl VlogWriter {
 	fn new () -> VlogWriter {
 		VlogWriter {
 			modules: vec![],
-			instances: vec![],
 			instance_counter: 0,
 		}
 	}
@@ -27,7 +27,9 @@ impl VlogWriter {
 			Some(s) => s.to_string(),
 			None => {
 				self.instance_counter += 1;
-				format!("i_{}", self.instance_counter)
+				let out = format!("i_{}", self.instance_counter);
+				println!("No instance name given, default to {}", out);
+				out
 			},
 		}
 	}
@@ -43,11 +45,11 @@ impl VlogWriter {
 		return ModuleRef::new(m);
 	}
 
-	fn module_get(&self, name : String) -> Option<ModuleRef> {
+	fn module_get(&self, name: &String) -> Option<ModuleRef> {
 		// TODO: this should be done more efficient, e.g. hash map as this is
 		// currently O(n)
 		for m in &self.modules {
-			if m.name == name {
+			if &m.name == name {
 				return Some(ModuleRef::new(m));
 			}
 		}
@@ -63,53 +65,146 @@ impl VlogWriter {
 		self.modules.get_mut(m_ref.id)
 	}
 
-	fn instance(&self, i_ref : &InstanceRef) -> Option<&Instance> {
-		self.instances.get(i_ref.id)
+	fn process_type(ty: &llhd::ty::Type) -> Type {
+		return match ty.as_ref() {
+			&llhd::ty::IntType(ref size) => {
+				assert!(*size > 0);
+				Rc::new(
+					TypeKind::Vector(*size, Rc::new(TypeKind::Scalar)))
+			},
+			&llhd::ty::SignalType(ref ty) => {
+				VlogWriter::process_type(ty)
+			},
+			&llhd::ty::VectorType(ref size, ref ty) => {
+				println!("VectorType found");
+				Rc::new(
+					TypeKind::Vector(*size,
+						VlogWriter::process_type(ty)))
+			},
+			_ => panic!("Unsupported type found!"),
+		};
 	}
 
-	fn instance_mut(&mut self, i_ref : &InstanceRef) -> Option<&mut Instance> {
-		self.instances.get_mut(i_ref.id)
+	fn process_entity_argument(m: &mut Module, arg: &llhd::Value, port_ty: PortType) -> () {
+		let name = arg.name().unwrap();
+		let ty = VlogWriter::process_type(&arg.ty());
+		let w_ref = m.wire_push(Wire::new(name.to_string(), ty));
+		m.port_push(Port::new(w_ref, port_ty));
 	}
 
-	fn process_entity(&mut self, module : &llhd::Module, entity: &llhd::Entity) -> () {
-		let m_ref = self.module_push(Module::new(entity.name().to_string()));
+	fn process_entity_arguments(&mut self, entity: &llhd::Entity, m_ref: &ModuleRef) -> () {
 		let mut m = self.module_mut(&m_ref).unwrap();
+		for arg in entity.inputs() {
+			VlogWriter::process_entity_argument(m, arg, PortType::Input);
+		}
+
+		for arg in entity.outputs() {
+			VlogWriter::process_entity_argument(m, arg, PortType::Output);
+		}
+	}
+
+	fn process_entity_inst_port_assign(&self, m: &Module, im: &Module, arg: &String, con: &String) -> PortAssign {
+		let port = im.port_get(con).unwrap();
+		let wire = m.wire_get(arg).unwrap();
+		PortAssign::new(port, wire)
+	}
+
+	fn process_entity_inst(&mut self, ctx: &llhd::ModuleContext, uctx: &llhd::UnitContext, m_ref: &ModuleRef, inst: &llhd::Inst, e: &llhd::ValueRef, ins: &Vec<llhd::ValueRef>, outs: &Vec<llhd::ValueRef>) -> Instance {
+		let target_name = uctx.value(&e).name().unwrap().to_string();
+		let inst_mod = match self.module_get(&target_name) {
+			Some(im) => {
+				im
+			}
+			None => {
+				panic!("Did not find module {:?}", &target_name)
+			}
+		};
+
+		let eref = match e {
+			&llhd::value::ValueRef::Entity(eref) => eref,
+			_ => panic!("Should be an entity"),
+		};
+		let inst_entity = ctx.entity(eref);
+		let inst_ectx = llhd::entity::EntityContext::new(ctx, inst_entity);
+		let inst_ctx = inst_ectx.as_unit_context();
+		let inst_name = self.inst_name(inst);
+		let im = self.module(&inst_mod).unwrap();
+		let mut instance = Instance::new(inst_name, inst_mod);
+
+		let m = self.module(m_ref).unwrap();
+
+		for (i, arg) in ins.iter().enumerate() {
+			let con_input = inst_entity.input(i);
+			let con_arg = inst_ctx.argument(con_input);
+			let con_value : &llhd::Value = con_arg;
+			let con_name = con_value.name().unwrap().to_string();
+			let arg_name = uctx.value(arg).name().unwrap().to_string();
+			let pa = self.process_entity_inst_port_assign(m, im, &arg_name, &con_name);
+			instance.port_assign_push(pa);
+		}
+
+		// uah... this is horrible
+		for (i, arg) in outs.iter().enumerate() {
+			let con_output = inst_entity.output(i);
+			let con_arg = inst_ctx.argument(con_output);
+			let con_value : &llhd::Value = con_arg;
+			let con_name = con_value.name().unwrap().to_string();
+			let arg_name = uctx.value(arg).name().unwrap().to_string();
+			let pa = self.process_entity_inst_port_assign(m, im, &arg_name, &con_name);
+			instance.port_assign_push(pa);
+		}
+
+		instance
+	}
+
+	fn process_entity_assign(&mut self, ctx: &llhd::UnitContext, m_ref: &ModuleRef, lhs: &llhd::ValueRef, rhs: &llhd::ValueRef) -> Assign {
+		let m = self.module_mut(&m_ref).unwrap();
+		let lhs_value = ctx.value(lhs).name().unwrap().to_string();
+		let lhs_w = m.wire_get(&lhs_value).unwrap();
+		let rhs_value = ctx.value(rhs).name().unwrap().to_string();
+		let rhs_w = m.wire_get(&rhs_value).unwrap();
+		Assign::new(lhs_w, rhs_w)
+	}
+
+	fn process_entity(&mut self, ctx: &llhd::ModuleContext, entity: &llhd::Entity) -> () {
+		let ectx = llhd::EntityContext::new(ctx, entity);
+		let uctx = ectx.as_unit_context();
+		let m_ref = self.module_push(Module::new(entity.name().to_string()));
+
+		self.process_entity_arguments(entity, &m_ref);
 
 		for inst in entity.insts() {
 			match inst.kind() {
+				&llhd::inst::InstKind::DriveInst(ref lhs, ref rhs, _) => {
+					let mut a = self.process_entity_assign(uctx, &m_ref, lhs, rhs);
+					let mut m = self.module_mut(&m_ref).unwrap();
+					m.assign_push(a);
+				},
 				&llhd::inst::InstKind::InstanceInst(_, ref target, ref ins, ref outs) => {
-					match target {
-						&llhd::value::ValueRef::Entity(e) => {
-							println!("{:?}", e);
-							let en = module.entity(e);
-							let inst_mod = match self.module_get(en.name().to_string()) {
-								Some(im) => {
-									im
-								}
-								None => {
-									panic!("Did not find module {:?}", en.name())
-								}
-							};
-
-							let inst_name = self.inst_name(inst);
-							m.instances.push(Instance::new(
-								inst_name,
-								inst_mod));
-						},
-						_ => panic!("Not an entity"),
-					}
+					let i = self.process_entity_inst(&ctx, uctx, &m_ref, inst, target, ins, outs);
+					let mut m = self.module_mut(&m_ref).unwrap();
+					m.instance_push(i);
+				},
+				&llhd::inst::InstKind::SignalInst(ref ty, _) => {
+					let value : &llhd::Value = inst;
+					let name = value.name().unwrap().to_string();
+					let ty = VlogWriter::process_type(ty);
+					let wire = Wire::new(name, ty);
+					let mut m = self.module_mut(&m_ref).unwrap();
+					m.wire_push(wire);
 				},
 				_ => println!("Not supported"),
 			}
 		}
 	}
 
-	fn from_llhd(&mut self, module : llhd::Module) -> () {
+	fn from_llhd(&mut self, module: llhd::Module) -> () {
+		let ctx = llhd::ModuleContext::new(&module);
 		for value in module.values() {
 			match value {
 				&llhd::ValueRef::Entity(e) => {
-					let entity = module.entity(e);
-					self.process_entity(&module, entity);
+					let entity = ctx.entity(e);
+					self.process_entity(&ctx, entity);
 				},
 				_ => println!("Something else"),
 			}
@@ -117,28 +212,46 @@ impl VlogWriter {
 	}
 
 	fn write_ports(&self, mut file : &File, m : &Module) {
-		for port in &m.ports {
-			write!(file, "port\n");
-		}
+		let ports = m.ports.iter().map(|ref port| {
+			format!("    {}", m.wire(&port.wire).unwrap().name)
+		})
+		.collect::<Vec<_>>()
+		.join(",\n");
+		write!(file, "{}\n", ports);
 	}
 
 	fn write_wires(&self, mut file : &File, m : &Module) {
 		for wire in &m.wires {
-			write!(file, "wire\n");
+			// TODO: vector dimensions are missing
+			write!(file, "  wire {};\n", wire.name);
 		}
 	}
 
 	fn write_instances(&self, mut file : &File, m : &Module) {
 		for i in &m.instances {
-			let m = self.module(&i.module).unwrap();
-			write!(file, "  {} {} (\n", m.name, i.inst_name);
+			let im = self.module(&i.module).unwrap();
+			write!(file, "  {} {} (\n", im.name, i.inst_name);
+
+			let ports = i.port_assigns.iter().map(|ref pa| {
+				let im = self.module(&i.module).unwrap();
+				let port = &im.wire(&im.port(&pa.lhs).unwrap().wire).unwrap().name;
+				let con = &m.wire(&pa.rhs).unwrap().name;
+				format!("    .{} ( {} )", port, con)
+			})
+			.collect::<Vec<_>>()
+			.join(",\n");
+
+			write!(file, "{}\n", ports);
+
 			write!(file, "  );\n");
 		}
 	}
 
 	fn write_assigns(&self, mut file : &File, m : &Module) {
 		for assign in &m.assigns {
-			write!(file, "assign\n");
+			let lhs = &m.wire(&assign.lhs).unwrap().name;
+			let rhs = &m.wire(&assign.rhs).unwrap().name;
+			write!(file, "  assign {} = {};\n", lhs, rhs);
 		}
 	}
 
@@ -148,7 +261,7 @@ impl VlogWriter {
 			Ok(f) => f,
 		};
 
-		file.write(b"// This file was generated by llhd-objcopy\n\n");
+		file.write(b"// This file was generated by llhd-objcopy\n\n").unwrap();
 
 		for m in &self.modules {
 			println!("{:?}", m);
@@ -160,8 +273,11 @@ impl VlogWriter {
 				write!(file, "module {};\n", m.name);
 			}
 
+			write!(file, "\n");
 			self.write_wires(&file, m);
+			write!(file, "\n");
 			self.write_instances(&file, m);
+			write!(file, "\n");
 			self.write_assigns(&file, m);
 			write!(file, "endmodule\n\n");
 		}
